9b9673319898a2dcbb0db93e8dc52a4da64a45ee
frirsta <frista-8@hotmail.com>
2022-12-03 06:10:49 +0000

    Add alert to signup function. When user creates an account an alert is shown on screen

--- users/views.py
+++ users/views.py
@@ -3,15 +3,20 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 
 from .forms import SignUpForm
+from django.contrib import messages
 
 
 def signup(request):
+    if request.user.is_authenticated:
+        return redirect('magazine:home')
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('magazine:home')
+
+            messages.success(request, "You are now a registered user!")
+            return redirect("magazine:home")
     else:
         form = SignUpForm()
-    return render(request, 'users/signup.html', {'form': form})
+    return render(request, "users/signup.html", {"form": form})
