0cd722707f4ceaaa688e063977aa2dd534fecb28
frirsta <frista-8@hotmail.com>
2022-12-15 10:14:45 +0000

    Change PostAdmin class

--- magazine/admin.py
+++ magazine/admin.py
@@ -3,13 +3,15 @@ from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
-@admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
     list_display = ('title', 'slug', 'created_on')
     list_filter = ("created_on", 'title', 'article_description')
     search_fields = ['title', 'content']
     prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content',)
+
+
+admin.site.register(Post, PostAdmin)
 
 
 @admin.register(Comment)
